96ce814d7ed815fd020a9ca15da48d66e2d9fbc0
VIVEK <vivek.170410107085@gmail.com>
2020-06-23 13:28:11 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -12,28 +12,10 @@
 <footer>
 	<div class="container-fluid padding">
 		<div class="row text-center">
-			<!--- Connect -->
-			<div class="container-fluid padding">
-				<div class="row text-center padding">
-					<div class="col-12">
-						<h2>connect</h2>
-					</div>
-					<div class="col-12 social padding">
-						<a href="#"><i class="fab fa-facebook"></i></a>
-						<a href="#"><i class="fab fa-twitter"></i></a>
-						<a href="#"><i class="fab fa-instagram"></i></a>
-						<a href="#"><i class="fab fa-youtube"></i></a>
-					</div>
-				</div>
-			</div>
-
 			<?php
 			if (isset($_SESSION['username'])) {
 			} else {
 			?>
-				<div class="col-md-4">
-					<p style="margin-top: 20px" ;><a href="about.php"><img src="about.jpg" style="border-radius: 50%"></a></p>
-				</div>
 				<div class="col-md-4">
 					<hr class="light">
 					<h5>Contact us</h5>
@@ -41,14 +23,14 @@
 					<p>NOVELREADER.85@gmail.com</p>
 					<p>8849-579-553</p>
 				</div>
-				<div class="col-md-4">
+				<div class="col-md-4 offset-md-4">
 					<h6>Subscribe</h6>
 					<div class="newsletter-form">
 						<form method="post" action="mail_process.php">
 							<div class="form-group">
 								<input type="email" name="subscriberemail" class="form-control newsletter-input" required placeholder="Enter Email Address" />
 							</div>
-							<button type="submit" name="emailsubscribe" class="btn btn-block">Subscribe <span class="angle_arrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span></button>
+							<button type="submit" name="emailsubscribe" class="btn btn-outline-dark">Subscribe <span class="angle_arrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span></button>
 						</form>
 						<p class="subscribed-text">*We'll inform you if any new book is uploaded.</p>
 					</div>
@@ -56,10 +38,18 @@
 			<?php
 			}
 			?>
+			<!--- Connect -->
+			<div class="container-fluid">
+				<div class="social ml-auto mr-auto">
+						<a href="https://www.twitter.com/Vivek2509_"><i class="fab fa-twitter"></i></a>
+						<a href="https://github.com/Vivek2509"><i class="fab fa-github"></i></a>
+						<a href="#"><i class="fab fa-instagram"></i></a>
+						<a href="https://in.pinterest.com/vivek2509/"><i class="fab fa-pinterest"></i></a>
+				</div>
+			</div>
 			<div class="col-12">
 				<hr class="light">
-				<h5>&copy;VIVEK PATEL</h5>
-				<h6>S.V.I.T VASAD</h6>
+				<h5>Made with <span></span>❤️ VIVEK PATEL</h5>
 			</div>
 		</div>
 	</div>
